e23cfe76d46a6413431481e62de48e6c061a0cf0
Luiz-Fernando-Policarpo-Leandro <fasfernandoal@outlook.com.br>
2026-03-16 15:45:16 -0300

    sql new

---
+++ db/db.sql
@@ -0,0 +1,70 @@
+CREATE DATABASE IF NOT EXISTS CMS CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE CMS;
+
+CREATE TABLE IF NOT EXISTS Preferencias (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo_landing VARCHAR(255) NOT NULL,
+    favicon VARCHAR(255) NOT NULL,
+    logo_cabecalho VARCHAR(255) NOT NULL,
+    facebook VARCHAR(255) NOT NULL,
+    twitter VARCHAR(255) NOT NULL,
+    instagram VARCHAR(255) NOT NULL,
+    titulo_secaoHome VARCHAR(255) NOT NULL,
+    subtitulo_secaoHome TEXT NOT NULL,
+    imagem_secaoHome VARCHAR(255) NOT NULL,
+    titulo_caracticasHome VARCHAR(255) NOT NULL,
+    titulo_secaoLojaApp VARCHAR(255) NOT NULL,
+    subtitulo_secaoLojaApp TEXT NOT NULL,
+    imagem_secaoLojaApp VARCHAR(255) NOT NULL,
+    image_AppStore VARCHAR(255) NOT NULL,
+    image_GooglePlay VARCHAR(255) NOT NULL,
+    telefone_contato VARCHAR(25) NOT NULL,
+    logo_rodape VARCHAR(255) NOT NULL,
+    mesagem_rodape TEXT NOT NULL,
+    url_rodape VARCHAR(255) NOT NULL,
+    mensagem_powered TEXT NOT NULL
+
+) ENGINE=InnoDB 
+DEFAULT CHARSET=utf8mb4 
+COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB 
+DEFAULT CHARSET=utf8mb4 
+COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS caracteristicas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    descricao TEXT NOT NULL
+) ENGINE=InnoDB 
+DEFAULT CHARSET=utf8mb4 
+COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE IF NOT EXISTS testemunhos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    funcao VARCHAR(255) NOT NULL,
+    titulo VARCHAR(255) NOT NULL,
+    descricao TEXT NOT NULL,
+    foto VARCHAR(255) NOT NULL,
+    imagem_fundo VARCHAR(255) NOT NULL
+) ENGINE=InnoDB 
+DEFAULT CHARSET=utf8mb4 
+COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS contactos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    telefone VARCHAR(25) NOT NULL,
+    mensagem TEXT NOT NULL,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB 
+DEFAULT CHARSET=utf8mb4 
+COLLATE=utf8mb4_0900_ai_ci;
